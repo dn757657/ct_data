6c5a34181ae5363f59bae1f4939378f5f108dd3a
dn757657 <daniel.js.campbell@gmail.com>
2022-01-05 18:56:34 -0700

    First commit ct_data

--- src/pricing.py
+++
@@ -1,9 +0,0 @@
-import pandas_datareader as datar
-
-
-def get_price(symbol, source='yahoo'):
-    """ give a symobl -> get a price """
-
-    price = datar.DataReader(symbol, source)
-    price = price["Close"][price.last_valid_index()]
-    return price
